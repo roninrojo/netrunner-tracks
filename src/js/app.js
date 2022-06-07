7d4e804beaf205d3b0ae1f489d3c7df8cb4ec9f7
acarazo <alberto.carazo@gmail.com>
2022-06-07 18:34:22 +0200

    numeros de click

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,65 @@
-// Vars
-const clicks = document.querySelector(".tracks-clicks");
+// Selectors
+
+const defaultClick = document.querySelector('span[data-number]');
+const clicksTrack = document.querySelector(".tracks-clicks__row");
+const clicks = document.querySelectorAll('span[data-number]');
 const clicksTitle = document.querySelector(".tracks-clicks > .section-title")
-console.log(clicksTitle);
+const resteClicks = document.querySelector("#reset-clicks");
+
+// Classes
+const User = {
+    type: '',
+}
 
 // Evenets
-
 document.addEventListener('DOMContentLoaded', () => {
-    clicks.addEventListener('click', addClassSelected);
+    clicksTrack.addEventListener('click', addClassSelected);
+    resteClicks.addEventListener('click', resetClicks);
 });
+
+// Functions
+const limpiarHTML = (nodo) => { while (nodo.firstChild) nodo.firstChild.remove() };
 
 function addClassSelected(e) {
     
-    const clicks = document.querySelectorAll('span[data-number]');
+    const currentClick = e.target.closest('.click');
     
+    if (currentClick != null) {
+        const currentNumber = currentClick.dataset.number;  
+        const arrClick = (Object.values(clicks)).filter(item => item.dataset.number != currentNumber);
 
-    clicks.forEach( click => click.classList.remove('selected'));
-    
-    e.target.classList.add('selected');
-    
+        arrClick.forEach(item => {
+            item.classList.remove('selected');
+            const lastNumber = item.querySelector('.click-number');
+            if (lastNumber) item.removeChild(lastNumber);
+        });
+
+        currentClick.classList.add('selected');
+        const lastNumber = document.querySelector('.click-number');
+        if (!lastNumber) {
+            const numClick = document.createElement('span');
+            numClick.classList.add('click-number');
+            numClick.textContent = currentNumber;
+            currentClick.appendChild(numClick);
+        }
+    } 
+}   
+
+function resetClicks(e) {
+    clicks.forEach((click) => {
+        const lastNumber = click.querySelector('.click-number');
+        if (Number(click.dataset.number) === 1) {
+            click.classList.add('selected');
+            if (!lastNumber) {
+                const numClick = document.createElement('span');
+                numClick.classList.add('click-number');
+                numClick.textContent = "1";
+                click.appendChild(numClick);    
+            }
+            
+        } else {
+            click.classList.remove('selected');
+            if (lastNumber) click.removeChild(lastNumber);
+        } 
+    })
 }
