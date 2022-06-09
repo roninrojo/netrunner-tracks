b4b391c704993d7aed9efb9c14abf1731e3a7c90
acarazo <alberto.carazo@gmail.com>
2022-06-09 13:51:00 +0200

    Augmentet ui + class buton + reset buton

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,80 @@
 // Selectors
 
+// Clicks
 const defaultClick = document.querySelector('span[data-number]');
 const clicksTrack = document.querySelector(".tracks-clicks__row");
 const clicks = document.querySelectorAll('span[data-number]');
 const clicksTitle = document.querySelector(".tracks-clicks > .section-title")
-const resteClicks = document.querySelector("#reset-clicks");
+const resetClicks = document.querySelector("#reset-clicks");
+
+// Tracks
+const addCreditTrigger = document.querySelector("#add-credit-button");
+const substractCreditTrigger = document.querySelector("#substract-credit-button");
+
+const addMemoryTrigger = document.querySelector("#add-memory-button");
+const substractMemoryTrigger = document.querySelector("#substract-memory-button");
+
+const addTagTrigger = document.querySelector("#add-tag-button");
+const substractTagTrigger = document.querySelector("#substract-tag-button");
+
+const creditsTarget = document.querySelector("#credits-number");
+const memoryTarget = document.querySelector("#memory-number");
+const tagsTarget = document.querySelector("#tags-number");
+
+const resetTracks = document.querySelectorAll(".button--reset");
+
+
+
+
 
 // Classes
-const User = {
-    type: '',
+
+class Button {
+    constructor(target) {
+        this.target = target;
+    }
+
+    add() {
+        this.target.value = Number(this.target.value) + 1;
+    }
+    
+    sub() {
+        let amount = Number(this.target.value) - 1;
+        (amount <= 0) ? amount = 0 : amount;
+        this.target.value = amount;
+    }
 }
+
+const creditsBtn = new Button(creditsTarget);
+const memoryBtn = new Button(memoryTarget);
+const tagsBtn = new Button(memoryTarget);
 
 // Evenets
 document.addEventListener('DOMContentLoaded', () => {
     clicksTrack.addEventListener('click', addClassSelected);
-    resteClicks.addEventListener('click', resetClicks);
+    // resetClicks.addEventListener('click', resetClick);
+    
+    resetTracks.forEach(item => {
+        item.addEventListener('click', resetTrack)
+    })
+
+    addCreditTrigger.addEventListener('click', () => buttonActions(creditsBtn, 'add'));
+    substractCreditTrigger.addEventListener('click', () => buttonActions(creditsBtn, 'sub'));
+
+    addMemoryTrigger.addEventListener('click', () => buttonActions(memoryBtn, 'add'));
+    substractMemoryTrigger.addEventListener('click', () => buttonActions(memoryBtn, 'sub'));
+
+    // addTagsTrigger.addEventListener('click', () => buttonActions(tagsBtn, 'add'));
+    // substractTagsTrigger.addEventListener('click', () => buttonActions(tagsBtn, 'sub'));
+
+    // addCreditTrigger.addEventListener('click', addCredits);
+    // substractCreditTrigger.addEventListener('click', subtractCredits);
 });
+
+function buttonActions(button, action) {
+    if(action === 'add') button.add();
+    if(action === 'sub') button.sub();
+}
 
 // Functions
 const limpiarHTML = (nodo) => { while (nodo.firstChild) nodo.firstChild.remove() };
@@ -45,7 +104,23 @@ function addClassSelected(e) {
     } 
 }   
 
-function resetClicks(e) {
+function resetTrack(e) {
+    e.stopPropagation();
+    resetTracks.forEach(item => {
+        const inputId = item.id.split('-');
+        const elementId = e.target.id.split('-');
+        
+        if (inputId[1] === elementId[1]) {
+            const input = item.parentNode.querySelector(`#${inputId[1]}-number`);
+            if (input === null && (elementId[1] === 'clicks')) {
+                resetClick();
+            } else {
+                input.value = 0;
+            }
+        }
+    })
+}
+function resetClick() {
     clicks.forEach((click) => {
         const lastNumber = click.querySelector('.click-number');
         if (Number(click.dataset.number) === 1) {
@@ -63,3 +138,15 @@ function resetClicks(e) {
         } 
     })
 }
+
+// Credits
+
+// function addCredits() {
+//     const credits = Number(addCreditTarget.value) + 1;
+//     addCreditTarget.value = credits;
+// }
+
+// function subtractCredits() {
+//     const credits = Number(addCreditTarget.value) - 1;
+//     addCreditTarget.value = credits;
+// }
